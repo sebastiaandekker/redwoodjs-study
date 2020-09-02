575c6a2538e29bdbfc7d8dd375d2dd2b4f305f6b
Sebastiaan Dekker <sebastiaan@Sebastiaans-MBP.codaisseur.local>
2020-09-02 15:28:12 +0200

    docs(posts): adding study comments to schema and services

--- api/src/graphql/posts.sdl.js
+++ api/src/graphql/posts.sdl.js
@@ -1,4 +1,6 @@
+// posts.sdl (Schema Definition File), this defines the endpoint graphql is going to present
 export const schema = gql`
+  # defining the Post type
   type Post {
     id: Int!
     title: String!
@@ -6,24 +8,34 @@ export const schema = gql`
     createdAt: DateTime!
   }
 
+  # defining 2 queries:
+  # posts, which returns all the posts
+  # post, which returns 1 post
   type Query {
     posts: [Post!]!
     post(id: Int!): Post
   }
 
+  # defining the CreatePost input type
   input CreatePostInput {
     title: String!
     body: String!
   }
 
+  # defining the Update Post input type
   input UpdatePostInput {
     title: String
     body: String
   }
 
+  # 3 defined CRUD mutations for the database
   type Mutation {
+    # these resolvers are defined in ../services/posts/posts.js
+    # createPost resolver will take the CreatePostInput and forward it to the database as a create post operation
     createPost(input: CreatePostInput!): Post!
+    # updatePost resolver will take the UpdatePostInput along with the id to forward it to the database as a update post operation
     updatePost(id: Int!, input: UpdatePostInput!): Post!
+    # deletePost resolver will take the id to forward it to the database as a delete post operation
     deletePost(id: Int!): Post!
   }
 `
