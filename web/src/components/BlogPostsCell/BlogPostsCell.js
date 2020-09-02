e59723a7f394b67081e04ec64610a5272e984bdd
Sebastiaan Dekker <sebastiaan@Sebastiaans-MBP.codaisseur.local>
2020-09-02 16:18:34 +0200

    refactor(blog-posts): adding study comments, correctly query on posts

--- web/src/components/BlogPostsCell/BlogPostsCell.js
+++ web/src/components/BlogPostsCell/BlogPostsCell.js
@@ -1,20 +1,25 @@
 // Cells are a declarative approach to data fetching and one of Redwood's signature modes of abstraction.
 // a Cell is just a higher-order component that executes a GraphQL query and manages its lifecycle.
 
+// gql query to get the data
 export const QUERY = gql`
   query BlogPostsQuery {
-    blogPosts {
+    posts {
       id
     }
   }
 `
 
+// renders when the data is loading
 export const Loading = () => <div>Loading...</div>
 
+// renders this if the data is empty (empty array or null)
 export const Empty = () => <div>Empty</div>
 
+// renders if there is an error on the back-end
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
+// renders if everything loaded correctly
 export const Success = ({ blogPosts }) => {
   return JSON.stringify(blogPosts)
 }
