789c70efde71855d127af3d8a914c73b89502ae9
Sebastiaan Dekker <sebastiaan@Sebastiaans-MacBook-Pro.local>
2020-09-03 11:16:35 +0200

    feat(contact): generated and add contact page

---
+++ web/src/pages/ContactPage/ContactPage.js
@@ -0,0 +1,12 @@
+import { Link, routes } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const ContactPage = () => {
+  return (
+    <BlogLayout>
+      <h1>ContactPage</h1>
+    </BlogLayout>
+  )
+}
+
+export default ContactPage
